47baf6b0ad4e52ebadb83f9b31c15d858f7d1da0
Mubashir Tanveer Ayon <ayon261261skylinegtr@gmail.com>
2024-10-17 12:45:34 +0600

    level transition, story

---
+++ scripts/level_transition.gd
@@ -0,0 +1,31 @@
+extends Control
+
+signal key_pressed
+var tween:Tween
+var continued:bool = false
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept") and not continued:
+		key_pressed.emit()
+		continued = true
+		hide()
+		if tween != null:
+			tween.stop()
+			tween.kill()
+
+func _ready()->void:
+	modulate.a = 0
+	
+
+func show_transition()->void:
+	modulate.a = 0
+	continued = false
+	
+	
+	await get_tree().create_tween().tween_property(self,"modulate:a",1,1).finished
+	
+	
+	tween = get_tree().create_tween()
+	
+	tween.tween_property($LowerLabel,"modulate:a",1,0.5)
+	tween.tween_property($LowerLabel,"modulate:a",0,0.5)
+	tween.set_loops()
--- scripts/main_control.gd
+++ scripts/main_control.gd
@@ -1,32 +1,53 @@
+
+
 extends Control
+@onready var json_dict = JSON.parse_string(FileAccess.get_file_as_string("res://data/story_separated.json"))
 
-
-func _ready():
-	show_story_line()
+signal text_showed
+signal story_showed
 
 func show_story_line()->void:
-	var json_array = JSON.parse_string(FileAccess.get_file_as_string("res://data/story.json"))
+	$AudioStreamPlayer.play()
+	
+	
 	for i in range(7):
+		
+		
 		var tween:Tween = get_tree().create_tween()
 		tween.tween_property($TextureRect,"modulate:a",1,1)
-		await tween.finished
-		tween.stop()
+		
 		
 		$TextureRect.texture = load("res://test/poster"+str((i+1)) +".png")
-		$Label.modulate.a = 0.0
 		
 		
-		$Label.text = json_array[i]
-		tween.tween_property($Label,"modulate:a",1,1)
+		#for j in range(json_dict[str(i)].size()):
+			#$Label.text = json_dict[str(i)][j]
+			#tween.tween_property($Label,"modulate:a",1,1)
+			#tween.tween_interval(json_dict[str(i)][j].length()/10.0)
+			#tween.tween_property($Label,"modulate:a",0,1.5)
+			#await tween.finished
 		await tween.finished
+		show_story_text(i)
+		await text_showed
 		tween.stop()
-		
-		await get_tree().create_timer(json_array[i].length()/90.0).timeout
-
-		tween.tween_property($Label,"modulate:a",0,1.5)
-		await tween.finished
-		tween.stop()
-		
+		tween.kill()
+		tween = get_tree().create_tween()
 		tween.tween_property($TextureRect,"modulate:a",0,2)
 		await tween.finished
 		tween.stop()
+		tween.kill()
+	story_showed.emit()
+
+
+func show_story_text(index:int):
+	var array = json_dict[str(index)]
+	for i in range(array.size()):
+		var tween:Tween = get_tree().create_tween()
+		$Label.text = array[i]
+		tween.tween_property($Label,"modulate:a",1,1)
+		tween.tween_interval(array[i].length()/10.0)
+		tween.tween_property($Label,"modulate:a",0,1.5)
+		await tween.finished
+		tween.stop()
+		tween.kill()
+	text_showed.emit()
